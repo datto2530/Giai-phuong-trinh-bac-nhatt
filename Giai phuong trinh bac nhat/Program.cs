c982171e10d19da5f59d36d117883b9a444990b8
Dattobias <152405844+datto2530@users.noreply.github.com>
2023-12-09 16:52:59 +0700

    Da xong

---
+++ Giai phuong trinh bac nhat/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace baitap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap so a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Nhap so b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            if (a != 0)
+            {
+                Console.WriteLine("Phuong trinh co nghiem" + -b / a);
+            }
+            else
+            {
+                if (b == 0) Console.WriteLine("Phuong trinh vo so nghiem");
+                else
+                {
+                    Console.WriteLine("Phuong trinh vo nghiem");
+                }
+            }
+        }
+    }
+}
+
